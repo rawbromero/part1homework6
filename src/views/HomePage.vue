25186cccc51cd2fab82d640f3d8259bbd473f219
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-10-07 20:05:02 -0500

    added style to categories

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,25 +1,25 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue';
-import useAPI from '@/composables/useAPI'
+import useAPI from '@/composables/useAPI';
+import { RouterLink } from 'vue-router';
 
-const { categories, getGategories } = useAPI()
+const { categories, getCategories } = useAPI()
 
 onMounted(async () => {
-  await getGategories()
-  
+  await getCategories()
 })
-  
-
 </script>
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-  <div v-for="category in categories" :key="category.id">
-    {{ category.name }}
-
-    
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+    <RouterLink v-for="category in categories" 
+    :key="category.id"
+    :to="`/question/category/${category.id}`"
+    class="bg-white-600 text-blue-500 text-center flex h-32 items-center justify-center rounded-lg border-4 border-red-500 py-3 font-bold uppercase hover:cursor-pointer hover:border-blue-600 hover:bg-purple-700-400 hover:text-orange-300 transition-colors duration-350">
+      {{ category.name }}
+    </RouterLink>
   </div>
-
+  
 </template>
-
