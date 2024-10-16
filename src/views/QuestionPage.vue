5e9b00b88e789c2a897bf84a50dfa264b7c876e4
rawbromero <129907865+rawbromero@users.noreply.github.com>
2024-10-15 19:35:01 -0500

    fixed questions

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
+import BaseTitle from '@/components/BaseTitle.vue';
 
 const api = useAPI()
 const question = ref(null)
@@ -17,6 +18,16 @@ onMounted(async () => {
 
 
 <template>
+
+  <div v-if="question" class="">
+<BaseTitle> {{ question.category }}</BaseTitle>
+{{ question.question }}
+
+  </div>
+  <div v-else class="">
+    loading...
+
+  </div>
 
    {{ question }}
 
